7f8ef18857af7a1eed9152c3af205b2bd41ced9d
ahmprgr <ahm.prgr.01bin.dev.89@gmail.com>
2026-07-07 15:12:54 +0330

    the old HeroSection jsx file deleted

--- src/components/home/HeroSection/Terminal/Main.jsx
+++ src/components/home/HeroSection/Terminal/Main.jsx
@@ -4,7 +4,7 @@ import Head from "./Head";
 export default function Main() {
   return (
     <div
-      class="lg:col-span-5 border border-[#3d0000] bg-[#000000] rounded-xl overflow-hidden font-mono text-xs text-slate-400 h-64 shadow-2xl relative text-left"
+      className={"lg:col-span-5 border border-[#3d0000] bg-[#000000] rounded-xl overflow-hidden font-mono text-xs text-slate-400 h-64 shadow-2xl relative text-left"}
       dir="ltr"
     >
       <Head />
