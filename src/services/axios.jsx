8d559f488da77c876c7845439ebb8b3256e11014
ahmprgr <ahm.prgr.01bin.dev.89@gmail.com>
2026-07-10 13:26:42 +0330

    add axios instace for fetch back-end api easily

---
+++ src/services/axios.jsx
@@ -0,0 +1,19 @@
+import axios from 'axios';
+import qs from 'qs';
+
+const axiosInstance = axios.create({
+  baseURL: 'http://localhost:1404/api',
+  timeout: 10000,
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded',
+  },
+
+  transformRequest: [(data) => {
+    if (data && typeof data === 'object') {
+      return qs.stringify(data);
+    }
+    return data;
+  }],
+});
+
+export default axiosInstance;
