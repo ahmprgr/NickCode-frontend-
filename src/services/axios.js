aa507c4fce4ea8decfb1d19ef228b72ee0dc584b
ahmprgr <ahm.prgr.01bin.dev.89@gmail.com>
2026-07-21 15:38:18 +0330

    add baseUrl and credentials to axios instance

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,19 +1,22 @@
-import axios from 'axios';
-import qs from 'qs';
+import axios from "axios";
+import qs from "qs";
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:1404/api',
+  baseURL: "http://localhost:1404/api",
   timeout: 10000,
+  withCredentials: true,
   headers: {
-    'Content-Type': 'application/x-www-form-urlencoded',
+    "Content-Type": "application/x-www-form-urlencoded",
   },
 
-  transformRequest: [(data) => {
-    if (data && typeof data === 'object') {
-      return qs.stringify(data);
-    }
-    return data;
-  }],
+  transformRequest: [
+    (data) => {
+      if (data && typeof data === "object") {
+        return qs.stringify(data);
+      }
+      return data;
+    },
+  ],
 });
 
 export default axiosInstance;
