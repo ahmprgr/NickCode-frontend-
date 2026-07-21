9c9314ae653e017555bb775f1e13e6b27778716e
ahmprgr <ahm.prgr.01bin.dev.89@gmail.com>
2026-07-21 16:43:20 +0330

    the admin roter added

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -2,23 +2,32 @@ import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/Home";
 import NotFound from "../pages/404";
 import Register from "../pages/auth/Register";
-import Login from "../pages/auth/Login"
+import Login from "../pages/auth/Login";
 import UserPanel from "../components/userPanel/Main";
-export const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/sign-up",
-    element: <Register />,
-  },
-  { path: "/sign-in", element: <Login /> },
-  {
-    path:"/user/dashboard",element: <UserPanel />
-  },
-  {
-    path: "*",
-    element: <NotFound />,
-  },
-],{ basename: "/NickCode-frontend-" },);
+import AdminPanel from "../components/adminPanel/Main";
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/sign-up",
+      element: <Register />,
+    },
+    { path: "/sign-in", element: <Login /> },
+    {
+      path: "/user/dashboard",
+      element: <UserPanel />,
+    },
+    {
+      path: "/admin/dashboard",
+      element: <AdminPanel />,
+    },
+    {
+      path: "*",
+      element: <NotFound />,
+    },
+  ],
+  { basename: "/NickCode-frontend-" },
+);
