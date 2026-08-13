f3f45e776c57f91b20bfca3f54208ae91191d598
ahmprgr <ahm.prgr.01bin.dev.89@gmail.com>
2026-08-13 16:48:59 +0330

    userpanel has done

--- src/components/userPanel/Main.jsx
+++ src/components/userPanel/Main.jsx
@@ -1,20 +1,46 @@
 import Header from "./../../layouts/Header/Main";
 import Footer from "./../../layouts/Footer/Main";
-import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
-
+import Sidebar from "./sidebar/Main";
+import InfoSection from "./infoSection/Main";
+import CourseStatus from "./courseStatusSection/Main";
+import { useEffect, useState } from "react";
+import { getMe } from "../../services/auth";
+import { useNavigate } from "react-router-dom";
+import { notify } from "./../../utils/ToastMessage";
 export default function Main() {
-  // const location = useLocation()
-  // useEffect(() => {
-  //   if (location.pathname === "/user/dashboard") {
-
-  //   }
-  // }, []);
-
+  const navigate = useNavigate();
+  let [user, setUser] = useState(null);
+  useEffect(() => {
+    (async () => {
+      try {
+        const result = await getMe();
+        setUser(result.data.user);
+        // if (result.data.user.role === "admin") {
+        //   navigate("/admin/dashboard");
+        // }
+      } catch (error) {
+        notify(
+          "نشست شما منقضی شده است",
+          error.response?.data?.message,
+          "warning",
+        );
+        navigate("/sign-in");
+      }
+    })();
+  }, []);
 
   return (
     <>
-      <Header />
+      <Header {...user }/>
+      <section class="bg-[#000000] text-white min-h-screen flex flex-col justify-between overflow-x-hidden select-none">
+        <main class="flex-grow pt-24 pb-12 px-4 max-w-7xl mx-auto w-full grid grid-cols-1 lg:grid-cols-4 gap-6">
+          <Sidebar {...user }/>
+          <div class="lg:col-span-3 space-y-6">
+            <InfoSection />
+            <CourseStatus />
+          </div>
+        </main>
+      </section>
       <Footer />
     </>
   );
